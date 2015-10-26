0760b0ef15e57b19de7e4ffd1234582694da0059
frank goossens <futtta@gmail.com>
2015-10-26 15:08:17 +0100

    fix for "Syntax error, unexpected T_SL"
    on older version of PHP, but still

--- classes/autoptimizeStyles.php
+++ classes/autoptimizeStyles.php
@@ -372,7 +372,7 @@ class autoptimizeStyles extends autoptimizeBase {
 			// CDN the fonts!
 			if ((!empty($this->cdn_url))&&apply_filters("autoptimize_filter_css_fonts_cdn",false)) {
 				$fontreplace = array();
-				$fonturl_regex = <<<'LOD'
+				$fonturl_regex = <<<LOD
 ~(?(DEFINE)(?<quoted_content>(["']) (?>[^"'\\]++ | \\{2} | \\. | (?!\g{-1})["'] )*+ \g{-1})(?<comment> /\* .*? \*/ ) (?<url_skip>(?: data: ) [^"'\s)}]*+ ) (?<other_content>(?> [^u}/"']++ | \g<quoted_content> | \g<comment> | \Bu | u(?!rl\s*+\() | /(?!\*) | \g<url_start> \g<url_skip> ["']?+ )++ ) (?<anchor> \G(?<!^) ["']?+ | @font-face \s*+ { ) (?<url_start> url\( \s*+ ["']?+ ) ) \g<comment> (*SKIP)(*FAIL) | \g<anchor> \g<other_content>?+ \g<url_start> \K ((?:(?:https?:)?(?://[[:alnum:]\-\.]+)(?::[0-9]+)?)?\/[^"'\s)}]*+) ~xs
 LOD;
 
