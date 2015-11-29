493b9ef6fca1e339e887787136a94ac57ecbd0ca
frank goossens <futtta@gmail.com>
2015-11-29 07:44:04 +0100

    switch to forked and fixed cssmin
    until Tubal provides official fixes.

--- autoptimize.php
+++ autoptimize.php
@@ -223,7 +223,7 @@ function autoptimize_start_buffering() {
 				}
 			} else {
 				if (!class_exists('CSSmin')) {
-					@include(AUTOPTIMIZE_PLUGIN_DIR.'classes/external/php/yui-php-cssmin-2.4.8-4.php');
+					@include(AUTOPTIMIZE_PLUGIN_DIR.'classes/external/php/yui-php-cssmin-2.4.8-4_fgo.php');
 				}
 			}
 			define('COMPRESS_CSS',false);
