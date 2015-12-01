1a9997456c1949aeb8867a24b05aa1415fd34cdd
frank goossens <futtta@gmail.com>
2015-12-01 16:21:47 +0100

    nogooglefont changes part 3

--- classes/autoptimizeStyles.php
+++ classes/autoptimizeStyles.php
@@ -29,7 +29,12 @@ class autoptimizeStyles extends autoptimizeBase {
 			$this->whitelist = array_filter(array_map('trim',explode(",",$whitelistCSS)));
 		}
 		
-		$removableCSS = apply_filters( 'autoptimize_filter_css_removables', '');
+		if ($options['nogooglefont'] == true) {
+			$removableCSS = "fonts.googleapis.com";
+		} else {
+			$removableCSS = "";
+		}
+		$removableCSS = apply_filters( 'autoptimize_filter_css_removables', $removableCSS);
 		if (!empty($removableCSS)) {
 			$this->cssremovables = array_filter(array_map('trim',explode(",",$removableCSS)));
 		}
