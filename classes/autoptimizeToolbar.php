ad9c1dd342fa938b3a8c19b7cb699362423912fd
Pablo Custo <pablocusto@gmail.com>
2016-05-02 09:34:54 -0300

    Added files via upload

---
+++ classes/autoptimizeToolbar.php
@@ -0,0 +1,34 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class autoptimizeToolbar {
+
+	public function __construct(){}
+
+	public function add(){
+		add_action( 'wp_before_admin_bar_render', array($this, 'autoptimize_admin_bar_menu') );
+	}
+
+	public function autoptimize_admin_bar_menu() {
+		global $wp_admin_bar;
+
+		$wp_admin_bar->add_menu( array(
+			'id'    => 'autoptimize',
+			'title' => __("Autoptimize",'autoptimize')
+		));
+
+		$wp_admin_bar->add_menu( array(
+			'id'    => 'autoptimize-delete-cache',
+			'title' => __("Delete Cache",'autoptimize'),
+			'parent'=> 'autoptimize'
+		));
+
+		?>
+		<script type="text/javascript">
+
+		</script>
+		<?php
+
+	}
+}
+?>
