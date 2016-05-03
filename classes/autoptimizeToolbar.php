248e51ad3af0f8565fa56c10579e421adb7c91a2
Pablo Custo <pablocusto@gmail.com>
2016-05-03 15:59:11 -0300

    Added files via upload

--- classes/autoptimizeToolbar.php
+++ classes/autoptimizeToolbar.php
@@ -3,13 +3,27 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class autoptimizeToolbar {
 
-	public function __construct(){}
-
-	public function add(){
-		add_action( 'wp_before_admin_bar_render', array($this, 'autoptimize_admin_bar_menu') );
+	public function __construct()
+	{
+		// Load admin toolbar feature
+		add_action( 'plugins_loaded', array( $this, 'load_toolbar' ) );
 	}
 
-	public function autoptimize_admin_bar_menu() {
+	public function load_toolbar()
+	{
+		if( current_user_can( 'manage_options' ) ){
+			
+			// Load custom admin styles and javascript
+			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+			
+			add_action( 'wp_ajax_autoptimize_delete_cache', array( $this, 'delete_cache' ) );
+
+			add_action( 'wp_before_admin_bar_render', array($this, 'add_toolbar') );
+		}
+	}
+
+	public function add_toolbar()
+	{
 		global $wp_admin_bar;
 
 		$wp_admin_bar->add_menu( array(
@@ -23,5 +37,18 @@ class autoptimizeToolbar {
 			'parent'=> 'autoptimize'
 		));
 	}
+
+	public function delete_cache()
+	{
+		autoptimizeCache::clearall();
+	}
+
+	public function enqueue_scripts()
+	{
+		wp_enqueue_style( 'autoptimize-toolbar', plugins_url( 'autoptimize/classes/static/css/toolbar.css' ), array(), time(), "all" );
+
+		wp_enqueue_script( 'autoptimize-toolbar', plugins_url( 'autoptimize/classes/static/js/toolbar.js' ), array(), time(), true );
+	}
+
 }
 ?>
