2e9f73a1c9fee741f510ff7642acd148ab191c55
Pablo Custo <pablocusto@gmail.com>
2016-05-05 03:21:32 -0300

    Added files via upload

--- classes/static/js/toolbar.js
+++ classes/static/js/toolbar.js
@@ -1,23 +1,27 @@
 jQuery( document ).ready(function() {
-	jQuery("body").append('<div id="autoptimize-loader-toolbar"></div>');
+	jQuery('body').append('<div id="autoptimize-loader-toolbar"></div>');
 
-	jQuery("#wp-admin-bar-autoptimize-default li").click(function(e){
-		var id = (typeof e.target.id != "undefined" && e.target.id) ? e.target.id : jQuery(e.target).parent("li").attr("id");
-		var action = "";
+	jQuery('#wp-admin-bar-autoptimize-default li').click(function(e){
+		var id = (typeof e.target.id != 'undefined' && e.target.id) ? e.target.id : jQuery(e.target).parent('li').attr('id');
+		var action = '';
 		
-		if(id == "wp-admin-bar-autoptimize-delete-cache"){
-			action = "autoptimize_delete_cache";
+		if(id == 'wp-admin-bar-autoptimize-delete-cache'){
+			action = 'autoptimize_delete_cache';
 		}
-		
-		jQuery("#autoptimize-loader-toolbar").show();
+
+		if( action == '' ) return;
+
+		jQuery('#autoptimize-loader-toolbar').show();
 		jQuery.ajax({
 			type: 'GET',
 			url: ajaxurl,
-			data : {"action": action},
-			dataType : "json",
+			data : {'action': action},
+			dataType : 'json',
 			cache: false, 
 			success: function(data){
-				jQuery("#autoptimize-loader-toolbar").hide();
+				jQuery('#autoptimize-loader-toolbar').hide();
+				jQuery('#wp-admin-bar-autoptimize-cache-info .size').html('0.00 B');
+				jQuery('#wp-admin-bar-autoptimize-cache-info .files').html('0');
 			}
 		});
 	});
