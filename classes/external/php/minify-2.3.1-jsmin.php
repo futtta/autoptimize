eaf501a6ce5ae0ef56b8eac9056616139a2d123f
frank goossens <futtta@gmail.com>
2016-07-20 16:47:34 +0200

    backport of mr. clay's fix
    cfr.
    https://github.com/mrclay/jsmin-php/commit/84e3cff6c49192c340b6bb97f28a3935e0c732cb

--- classes/external/php/minify-2.3.1-jsmin.php
+++ classes/external/php/minify-2.3.1-jsmin.php
@@ -194,27 +194,28 @@ class JSMin {
                 // fallthrough intentional
             case self::ACTION_DELETE_A: // 2
                 $this->a = $this->b;
-                if ($this->a === "'" || $this->a === '"' || $this->a ==="`") { // string literal
+                if ($this->a === "'" || $this->a === '"' || $this->a === '`') { // string/template literal
+                    $delimiter = $this->a;
                     $str = $this->a; // in case needed for exception
                     for(;;) {
                         $this->output .= $this->a;
                         $this->lastByteOut = $this->a;
-
                         $this->a = $this->get();
                         if ($this->a === $this->b) { // end quote
                             break;
                         }
-                        if ($this->isEOF($this->a)) {
+                        if ($delimiter === '`' && $this->a === "\n") {
+                            // leave the newline
+                        } elseif ($this->isEOF($this->a)) {
                             $byte = $this->inputIndex - 1;
-                            throw new JSMin_UnterminatedStringException(
+                            throw new UnterminatedStringException(
                                 "JSMin: Unterminated String at byte {$byte}: {$str}");
                         }
                         $str .= $this->a;
                         if ($this->a === '\\') {
                             $this->output .= $this->a;
                             $this->lastByteOut = $this->a;
-
-                            $this->a       = $this->get();
+                            $this->a = $this->get();
                             $str .= $this->a;
                         }
                     }
