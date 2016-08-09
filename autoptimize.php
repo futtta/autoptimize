30e3986e5da98fb472a613b22f78c18d86bae685
frank goossens <futtta@gmail.com>
2016-08-09 12:46:33 +0200

    temp. version change
    to make sure people see this is the to-be 2.1.0 but still allow the
    plugin to be updated when 2.1.0 is officially released.

--- autoptimize.php
+++ autoptimize.php
@@ -3,7 +3,7 @@
 Plugin Name: Autoptimize
 Plugin URI: http://blog.futtta.be/autoptimize
 Description: Optimizes your website, concatenating the CSS and JavaScript code, and compressing it.
-Version: 2.0.2
+Version: 2.0.99
 Author: Frank Goossens (futtta)
 Author URI: http://blog.futtta.be/
 Domain Path: localization/
