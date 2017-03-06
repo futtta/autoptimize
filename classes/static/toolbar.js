542da4bee96632151a95030fe2b588b1008ecb54
zytzagoo <zytzagoo@gmail.com>
2017-03-06 15:31:07 +0100

    Fix ajax cache clear not working + show success notice only when cache is actually cleared + whitespace/style

--- classes/static/toolbar.js
+++ classes/static/toolbar.js
@@ -1,13 +1,12 @@
 jQuery( document ).ready(function()
 {
-
 	var percentage = jQuery( '#wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar' ).attr('percentage');
 	var rotate = percentage * 1.8;
 
 	jQuery( '#wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .mask.full, #wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .fill' ).css({
-		'-webkit-transform'	: 'rotate(' + rotate + 'deg)',
-		'-ms-transform'		: 'rotate(' + rotate + 'deg)',
-		'transform'		: 'rotate(' + rotate + 'deg)'
+		'-webkit-transform' : 'rotate(' + rotate + 'deg)',
+		'-ms-transform'     : 'rotate(' + rotate + 'deg)',
+		'transform'         : 'rotate(' + rotate + 'deg)'
 	});
 
 	// Fix Background color of circle percentage & delete cache to fit with the current color theme
@@ -30,34 +29,45 @@ jQuery( document ).ready(function()
 
 		// Create and Show the Autoptimize Loading Modal
 		var modal_loading = jQuery( '<div class="autoptimize-loading"></div>' ).appendTo( 'body' ).show();
-                
+
+		var success = function() {
+			// Reset output values & class names of cache info
+			jQuery( '#wp-admin-bar-autoptimize-cache-info .size' ).attr( 'class', 'size green' ).html( '0.00 B' );
+			jQuery( '#wp-admin-bar-autoptimize-cache-info .files' ).html( '0' );
+			jQuery( '#wp-admin-bar-autoptimize-cache-info .percentage .numbers' ).attr( 'class', 'numbers green' ).html( '0%' );
+			jQuery( '#wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .fill' ).attr( 'class', 'fill bg-green' );
+
+			// Reset the class names of bullet icon
+			jQuery( '#wp-admin-bar-autoptimize' ).attr( 'class', 'menupop bullet-green' );
+
+			// Reset the Radial Bar progress
+			jQuery( '#wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .mask.full, #wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .fill' ).css({
+				'-webkit-transform'    : 'rotate(0deg)',
+				'-ms-transform'        : 'rotate(0deg)',
+				'transform'            : 'rotate(0deg)'
+			});
+		};
+
+		var notice = function() {
+			jQuery( '<div id="ao-delete-cache-timeout" class="notice notice-error is-dismissible"><p><strong><span style="display:block;clear:both;">' + autoptimize_ajax_object.error_msg + '</span></strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">' +  autoptimize_ajax_object.dismiss_msg + '</span></button></div><br>' ).insertAfter( '#wpbody .wrap h1:first-of-type' ).show();
+		};
+
 		jQuery.ajax({
-			type	: 'GET',
-			url	: autoptimize_ajax_object.ajaxurl,
-			data	: {'action':action, 'nonce':autoptimize_ajax_object.nonce},
-			dataType: 'json',
-			cache	: false,
-			timeout : 5000,
-			success	: function( data )
+			type     : 'GET',
+			url      : autoptimize_ajax_object.ajaxurl,
+			data     : {'action':action, 'nonce':autoptimize_ajax_object.nonce},
+			dataType : 'json',
+			cache    : false,
+			timeout  : 5000,
+			success  : function( cleared )
 			{
 				// Remove the Autoptimize Loading Modal
 				modal_loading.remove();
-
-				// Reset output values & class names of cache info
-				jQuery( '#wp-admin-bar-autoptimize-cache-info .size' ).attr( 'class', 'size green' ).html( '0.00 B' );
-				jQuery( '#wp-admin-bar-autoptimize-cache-info .files' ).html( '0' );
-				jQuery( '#wp-admin-bar-autoptimize-cache-info .percentage .numbers' ).attr( 'class', 'numbers green' ).html( '0%' );
-				jQuery( '#wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .fill' ).attr( 'class', 'fill bg-green' );
-
-				// Reset the class names of bullet icon 
-				jQuery( '#wp-admin-bar-autoptimize' ).attr( 'class', 'menupop bullet-green' );
-
-				// Reset the Radial Bar progress
-				jQuery( '#wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .mask.full, #wp-admin-bar-autoptimize-cache-info .autoptimize-radial-bar .fill' ).css({
-					'-webkit-transform'	: 'rotate(0deg)',
-					'-ms-transform'		: 'rotate(0deg)',
-					'transform'		: 'rotate(0deg)'
-				});
+				if ( cleared ) {
+					success();
+				} else {
+					notice();
+				}
 			},
 			error: function( jqXHR, textStatus )
 			{
@@ -65,8 +75,7 @@ jQuery( document ).ready(function()
 				modal_loading.remove();
 
 				// WordPress Admin Notice
-				jQuery( '<div id="ao-delete-cache-timeout" class="notice notice-error is-dismissible"><p><strong><span style="display:block;clear:both;">' + autoptimize_ajax_object.error_msg + '</span></strong></p><button type="button" class="notice-dismiss"><span class="screen-reader-text">' +  autoptimize_ajax_object.dismiss_msg + '</span></button></div><br>' ).insertAfter( '#wpbody .wrap h1:first-of-type' ).show();
-
+				notice();
 			}
 		});
 	});
