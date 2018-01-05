8a62a248e8f338f6a3ddaa8ee4aaba78e995e60d
futtta <futtta@gmail.com>
2018-01-05 06:58:23 +0100

    up to 2.3.2

--- autoptimize.php
+++ autoptimize.php
@@ -3,7 +3,7 @@
 Plugin Name: Autoptimize
 Plugin URI: https://autoptimize.com/
 Description: Optimizes your website, concatenating the CSS and JavaScript code, and compressing it.
-Version: 2.3.1
+Version: 2.3.2
 Author: Frank Goossens (futtta)
 Author URI: https://autoptimize.com/
 Domain Path: localization/
