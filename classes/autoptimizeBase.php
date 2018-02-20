6153763de2715d2989a24cb31f541c23f7c26315
futtta <futtta@gmail.com>
2018-02-20 11:24:57 +0100

    we're not in autoptimizeStyles.php any more ..

--- classes/autoptimizeBase.php
+++ classes/autoptimizeBase.php
@@ -379,7 +379,7 @@ abstract class autoptimizeBase {
 				}
 			} else if ($codeType === "css") {
                 // make sure paths to background images/ imported css/ fonts .. are OK
-                $_toMinify = $this->fixurls($pathIn,$_toMinify);
+                $_toMinify = autoptimizeStyles::fixurls($pathIn,$_toMinify);
                 if (class_exists('Minify_CSS_Compressor')) {
 					$tmp_code = trim(Minify_CSS_Compressor::process($_toMinify));
                 } else if(class_exists('CSSmin')) {
