3c6d273eeb48fc2fb1be0032920f112afe48398d
futtta <futtta@gmail.com>
2018-07-29 08:09:35 +0200

    Excluded scripts are not aggregated, but are (in AO 2.4) minified (if not already).

--- classes/autoptimizeConfig.php
+++ classes/autoptimizeConfig.php
@@ -261,7 +261,7 @@ input[type=url]:invalid {color: red; border-color:red;} .form-table th{font-weig
 <tr valign="top" class="<?php echo $hiddenClass;?>js_sub ao_adv js_aggregate">
 <th scope="row"><?php _e('Exclude scripts from Autoptimize:','autoptimize'); ?></th>
 <td><label><input type="text" style="width:100%;" name="autoptimize_js_exclude" value="<?php echo get_option('autoptimize_js_exclude',"seal.js, js/jquery/jquery.js"); ?>"/><br />
-<?php _e('A comma-separated list of scripts you want to exclude from being optimized, for example \'whatever.js, another.js\' (without the quotes) to exclude those scripts from being aggregated and minimized by Autoptimize.','autoptimize'); ?></label></td>
+<?php _e('A comma-separated list of scripts you want to exclude from being optimized, for example \'whatever.js, another.js\' (without the quotes) to exclude those scripts from being aggregated by Autoptimize.','autoptimize'); ?></label></td>
 </tr>
 <tr valign="top" class="<?php echo $hiddenClass;?>js_sub ao_adv js_aggregate">
 <th scope="row"><?php _e('Add try-catch wrapping?','autoptimize'); ?></th>
