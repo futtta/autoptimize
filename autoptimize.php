92707d9b4a9e589e6fe4b3e7c7336ba3b1677be8
futtta <futtta@gmail.com>
2018-09-28 23:50:30 +0200

    the to-be(come) 2.4.1, beta for now

--- autoptimize.php
+++ autoptimize.php
@@ -3,7 +3,7 @@
 Plugin Name: Autoptimize
 Plugin URI: https://autoptimize.com/
 Description: Optimizes your website, concatenating the CSS and JavaScript code, and compressing it.
-Version: 2.4.0-beta-5
+Version: 2.4.1-beta-1
 Author: Frank Goossens (futtta)
 Author URI: https://autoptimize.com/
 Text Domain: autoptimize
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'AUTOPTIMIZE_PLUGIN_VERSION', '2.4.0-beta-5' );
+define( 'AUTOPTIMIZE_PLUGIN_VERSION', '2.4.1-beta-1' );
 
 // plugin_dir_path() returns the trailing slash!
 define( 'AUTOPTIMIZE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Donate link: http://blog.futtta.be/2013/10/21/do-not-donate-to-me/
 Requires at least: 4.0
 Tested up to: 4.9
 Requires PHP: 5.3
-Stable tag: 2.4.0-beta-5
+Stable tag: 2.4.1-beta-1
 
 Autoptimize (Beta) speeds up your website by optimizing JS, CSS, HTML, Google Fonts and images, async-ing JS, removing emoji cruft and more.
 
