3442ff5b4e558cd2c8808172248735f1fa95cafc
frank goossens <futtta@gmail.com>
2019-03-01 11:36:45 +0100

    webp JS: make sure attrib is not null

--- classes/autoptimizeImages.php
+++ classes/autoptimizeImages.php
@@ -894,7 +894,7 @@ class autoptimizeImages
         // And add webp detection and loading JS.
         if ( $this->should_webp() ) {
             $_webp_detect = "function c_webp(A){var n=new Image;n.onload=function(){var e=0<n.width&&0<n.height;A(e)},n.onerror=function(){A(!1)},n.src='data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA=='}function s_webp(e){window.supportsWebP=e}c_webp(s_webp);";
-            $_webp_load   = "document.addEventListener('lazybeforeunveil',function({target:a}){supportsWebP&&['data-src','data-srcset'].forEach(function(b){attr=a.getAttribute(b),a.setAttribute(b,attr.replace(/\/client\//,'/client/to_webp,'))})});";
+            $_webp_load   = "document.addEventListener('lazybeforeunveil',function({target:c}){supportsWebP&&['data-src','data-srcset'].forEach(function(a){attr=c.getAttribute(a),null!==attr&&c.setAttribute(a,attr.replace(/\/client\//,'/client/to_webp,'))})});";
             echo apply_filters( 'autoptimize_filter_imgopt_webp_js', '<script' . $noptimize_flag . '>' . $_webp_detect . $_webp_load . '</script>' );
         }
     }
