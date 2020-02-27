cc4c42f0f7edd3c5dac96c25cc38f154916b3535
futtta <futtta@gmail.com>
2020-02-27 10:29:24 +0100

    Only run Extra optimizations if general conditions for optimization are met. This is to avoid e.g. removing query-strings is done even when optimizations are supposed to be off (e.g. as per "optimize for logged in users" setting being off).

--- classes/autoptimizeExtra.php
+++ classes/autoptimizeExtra.php
@@ -64,7 +64,7 @@ class autoptimizeExtra
             }
             add_filter( 'autoptimize_filter_settingsscreen_tabs', array( $this, 'add_extra_tab' ) );
         } else {
-            $this->run_on_frontend();
+            add_action( 'wp', array( $this, 'run_on_frontend' ) );
         }
     }
 
@@ -142,8 +142,16 @@ class autoptimizeExtra
         return $merged;
     }
 
-    protected function run_on_frontend()
+    public function run_on_frontend()
     {
+        // only run the Extra optimizations on frontend if general conditions
+        // for optimizations are met, this to ensure e.g. removing querystrings
+        // is not done when optimizing for logged in users is off, breaking
+        // some pagebuilders (Divi & Elementor).
+        if ( false === autoptimizeMain::should_buffer() ) {
+            return;
+        }
+
         $options = $this->options;
 
         // Disable emojis if specified.
