4989d5682c404ce507cf377cfe1b5a232f11a313
futtta <futtta@gmail.com>
2020-04-29 12:14:21 +0200

    Also exclude user account page which can have payment methods which can break as described by Mark Aaron.

--- classes/autoptimizeMain.php
+++ classes/autoptimizeMain.php
@@ -353,7 +353,7 @@ class autoptimizeMain
             // If setting says not to optimize cart/checkout.
             if ( false === $ao_noptimize && 'on' !== autoptimizeOptionWrapper::get_option( 'autoptimize_optimize_checkout', 'off' ) ) {
                 // Checking for woocommerce, easy digital downloads and wp ecommerce...
-                foreach ( array( 'is_checkout', 'is_cart', 'edd_is_checkout', 'wpsc_is_cart', 'wpsc_is_checkout' ) as $func ) {
+                foreach ( array( 'is_checkout', 'is_cart', 'is_account_page', 'edd_is_checkout', 'wpsc_is_cart', 'wpsc_is_checkout' ) as $func ) {
                     if ( function_exists( $func ) && $func() ) {
                         $ao_noptimize = true;
                         break;
