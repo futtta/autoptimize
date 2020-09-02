83c423b63bcee522ec90ba7f9c3a15ad05b8505c
futtta <futtta@gmail.com>
2020-09-02 14:25:27 +0200

    Change JS hashes for WordPress 5.5.1.

--- tests/test-ao.php
+++ tests/test-ao.php
@@ -310,7 +310,7 @@ MARKUP;
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/m-mobilemenu.js'></script>
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/main.js'></script>
 
-<script defer src="$cdnurl/${subfolder}${jspart}c10a3ded8515331810ba81308bdd18dd.js"></script></body>
+<script defer src="$cdnurl/${subfolder}${jspart}8db7472e1ba26a5b2a7c5ddca8e488e9.js"></script></body>
 </html>
 MARKUP;
 
@@ -379,7 +379,7 @@ MARKUP;
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/m-mobilemenu.js'></script>
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/main.js'></script>
 
-<script defer src="$cdnurl/${subfolder}${jspart}c10a3ded8515331810ba81308bdd18dd.js"></script></body>
+<script defer src="$cdnurl/${subfolder}${jspart}8db7472e1ba26a5b2a7c5ddca8e488e9.js"></script></body>
 </html>
 MARKUP;
 
@@ -448,7 +448,7 @@ MARKUP;
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/m-mobilemenu.js'></script>
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/main.js'></script>
 
-<script defer src="$cdnurl/${subfolder}${jspart}c10a3ded8515331810ba81308bdd18dd.js"></script></body>
+<script defer src="$cdnurl/${subfolder}${jspart}8db7472e1ba26a5b2a7c5ddca8e488e9.js"></script></body>
 </html>
 MARKUP;
 
@@ -517,7 +517,7 @@ MARKUP;
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/m-mobilemenu.js'></script>
 <script type='text/javascript' src='$siteurl/wp-content/themes/my-theme/js/main.js'></script>
 
-<script defer src="$cdnurl/${subfolder}${jspart}c10a3ded8515331810ba81308bdd18dd.js"></script></body>
+<script defer src="$cdnurl/${subfolder}${jspart}8db7472e1ba26a5b2a7c5ddca8e488e9.js"></script></body>
 </html>
 MARKUP;
 
