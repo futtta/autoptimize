9ea219fd327d3fa9d069d9fc7c10bc1249b8a019
futtta <futtta@gmail.com>
2020-12-11 15:36:09 +0100

    add default imgopt exclusion value (empty).

--- classes/autoptimizeConfig.php
+++ classes/autoptimizeConfig.php
@@ -812,6 +812,7 @@ echo __( 'A comma-separated list of CSS you want to exclude from being optimized
     {
         $defaults = array(
             'autoptimize_imgopt_checkbox_field_1' => '0', // imgopt off.
+            'autoptimize_imgopt_text_field_6'     => '',  // optimization exclusions.
             'autoptimize_imgopt_select_field_2'   => '2', // quality glossy.
             'autoptimize_imgopt_checkbox_field_3' => '0', // lazy load off.
             'autoptimize_imgopt_checkbox_field_4' => '0', // webp off (might be removed).
