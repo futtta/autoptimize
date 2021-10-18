c1805380b83d070ab773270b10f088ba6dc55488
shakee93 <shakee.zats@gmail.com>
2021-10-18 18:17:34 +0530

    feat: add deactivate survey on plugins page

---
+++ classes/autoptimizeExitSurvey.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Handles disk-cache-related operations.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class autoptimizeExitSurvey
+{
+
+    function __construct() {
+
+        global $pagenow;
+
+        if($pagenow != 'plugins.php'){
+
+            return;
+
+        }
+
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_survey_scripts']);
+
+        add_action('admin_footer', [$this, 'render_survey_model']);
+
+    }
+
+    function enqueue_survey_scripts(){
+
+        wp_enqueue_script( 'ao_exit_survey',  plugins_url( '/static/exit-survey/exit-survey.js', __FILE__ ), array(
+            'jquery'
+        ),AUTOPTIMIZE_PLUGIN_VERSION );
+
+        wp_enqueue_style('ao_exit_survey', plugins_url( '/static/exit-survey/exit-survey.css', __FILE__ ), null, AUTOPTIMIZE_PLUGIN_VERSION);
+    }
+
+    function render_survey_model(){
+
+        include_once 'static/exit-survey/exit-survey-model.php';
+
+    }
+
+}
---
+++ classes/static/exit-survey/exit-survey.js
@@ -0,0 +1,93 @@
+(function ($) {
+    $(document).ready(function () {
+        var targetElement = 'tr[data-slug="autoptimize"] span.deactivate a';
+        var redirectUrl = $(targetElement).attr('href');
+        if ($('.ao-feedback-overlay').length === 0) {
+            $('body').prepend('<div class="ao-feedback-overlay"></div>');
+        }
+        $('#ao_uninstall_feedback_popup').appendTo($(targetElement).parent());
+
+        $(targetElement).on('click', function (e) {
+            e.preventDefault();
+            $('#ao_uninstall_feedback_popup ').addClass('active');
+            $('body').addClass('ao-feedback-open');
+            $('.ao-feedback-overlay').on('click', function () {
+                $('#ao_uninstall_feedback_popup ').removeClass('active');
+                $('body').removeClass('ao-feedback-open');
+            });
+        });
+
+        $('#ao_uninstall_feedback_popup .info-disclosure-link').on('click', function (e) {
+            e.preventDefault();
+            $(this).parent().find('.info-disclosure-content').toggleClass('active');
+        });
+
+        $('#ao_uninstall_feedback_popup input[type="radio"]').on('change', function () {
+            var radio = $(this);
+            if (radio.parent().find('textarea').length > 0 &&
+                radio.parent().find('textarea').val().length === 0) {
+                $('#ao_uninstall_feedback_popup #ao-deactivate-yes').attr('disabled', 'disabled');
+                radio.parent().find('textarea').on('keyup', function (e) {
+                    if ($(this).val().length === 0) {
+                        $('#ao_uninstall_feedback_popup #ao-deactivate-yes').attr('disabled', 'disabled');
+                    } else {
+                        $('#ao_uninstall_feedback_popup #ao-deactivate-yes').removeAttr('disabled');
+                    }
+                });
+            } else {
+                $('#ao_uninstall_feedback_popup #ao-deactivate-yes').removeAttr('disabled');
+            }
+        });
+
+        $('#ao_uninstall_feedback_popup #ao-deactivate-no').on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $(targetElement).unbind('click');
+            $('body').removeClass('ao-feedback-open');
+            $('#ao_uninstall_feedback_popup').remove();
+            if (redirectUrl !== '') {
+                location.href = redirectUrl;
+            }
+        });
+
+        $('#ao_uninstall_feedback_popup #ao-deactivate-yes').on('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            $(targetElement).unbind('click');
+            var selectedOption = $(
+                '#ao_uninstall_feedback_popup input[name="ao-deactivate-option"]:checked');
+
+            var reason;
+
+            if(selectedOption.attr("id") === "ao_feedback999"){
+                reason = selectedOption.parent().find('textarea').val().trim()
+            }else{
+                reason = selectedOption.parent().find('label').text().trim()
+            }
+
+            var data = {
+                'url': null,
+                'reason': reason,
+                'type': 'wordpress',
+                'version' : $('#ao_plugin_version').text().trim(),
+            };
+            $.ajax({
+                type: 'POST',
+                url: 'https://misc.optimizingmatters.com/ao_exit_survey/index.php',
+                data: data,
+                complete() {
+                    $('body').removeClass('ao-feedback-open');
+                    $('#ao_uninstall_feedback_popup').remove();
+                    if (redirectUrl !== '') {
+                        location.href = redirectUrl;
+                    }
+                },
+                beforeSend() {
+                    $('#ao_uninstall_feedback_popup').addClass('sending-feedback');
+                    $('#ao_uninstall_feedback_popup .popup--footer').remove();
+                    $('#ao_uninstall_feedback_popup .popup--body').html('<i class="dashicons dashicons-update-alt"></i>');
+                }
+            });
+        });
+    });
+})(jQuery);
