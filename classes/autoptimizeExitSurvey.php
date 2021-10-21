00dc266a18f7e489ef6500df5f53234c4ed1231b
frank goossens <futtta@gmail.com>
2021-10-21 16:05:10 +0200

    small coding style changes.

--- classes/autoptimizeExitSurvey.php
+++ classes/autoptimizeExitSurvey.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Handles disk-cache-related operations.
+ * Add exit-survey logic to plugins-page.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -9,36 +9,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class autoptimizeExitSurvey
 {
-
     function __construct() {
-
         global $pagenow;
 
-        if($pagenow != 'plugins.php'){
-
+        if ( $pagenow != 'plugins.php' ) {
             return;
-
         }
 
-        add_action('admin_enqueue_scripts', [$this, 'enqueue_survey_scripts']);
-
-        add_action('admin_footer', [$this, 'render_survey_model']);
-
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_survey_scripts' ) );
+        add_action( 'admin_footer', array( $this, 'render_survey_model' ) );
     }
 
-    function enqueue_survey_scripts(){
-
+    function enqueue_survey_scripts() {
         wp_enqueue_script( 'ao_exit_survey',  plugins_url( '/static/exit-survey/exit-survey.js', __FILE__ ), array(
             'jquery'
-        ),AUTOPTIMIZE_PLUGIN_VERSION );
+        ), AUTOPTIMIZE_PLUGIN_VERSION );
 
-        wp_enqueue_style('ao_exit_survey', plugins_url( '/static/exit-survey/exit-survey.css', __FILE__ ), null, AUTOPTIMIZE_PLUGIN_VERSION);
+        wp_enqueue_style( 'ao_exit_survey', plugins_url( '/static/exit-survey/exit-survey.css', __FILE__ ), null, AUTOPTIMIZE_PLUGIN_VERSION );
     }
 
-    function render_survey_model(){
-
+    function render_survey_model() {
         include_once 'static/exit-survey/exit-survey-model.php';
-
     }
-
 }
