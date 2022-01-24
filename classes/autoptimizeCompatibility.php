1a2a59a8fad9a7ddf71691c3b623ff30f03a6322
frank goossens <futtta@gmail.com>
2022-01-24 20:55:04 +0100

    misc changes:
    * only run on frontend
    * add filter to disable elementor compat helper
    * revslider compat helper

--- classes/autoptimizeCompatibility.php
+++ classes/autoptimizeCompatibility.php
@@ -16,7 +16,10 @@ class autoptimizeCompatibility
      */
     public function __construct()
     {
-        $this->run();
+        if ( ! is_admin() ) {
+            $this->conf = autoptimizeConfig::instance();
+            $this->run();
+        }
     }
 
     /**
@@ -26,8 +29,37 @@ class autoptimizeCompatibility
     public function run()
     {
         // Edit with Elementor in frontend admin menu (so for editors/ administrators) needs JS opt. disabled to appear & function.
-        if ( defined( 'ELEMENTOR_VERSION' ) && is_user_logged_in() && current_user_can( 'edit_posts' ) ) {
+        if ( defined( 'ELEMENTOR_VERSION' ) && is_user_logged_in() && current_user_can( 'edit_posts' ) && apply_filters( 'autoptimize_filter_compatibility_editelementor_active', true ) ) {
             add_filter( 'autoptimize_filter_js_noptimize', '__return_true' );
         }
+        
+        // revslider; jQuery should not be deferred + exclude all revslider JS.
+        if ( defined( 'RS_REVISION' ) && $this->conf->get( 'autoptimize_js' ) && true == $this->revslider_compat_helper() && apply_filters( 'autoptimize_filter_compatibility_revslider_active', true ) ) {
+            add_filter( 'autoptimize_filter_js_exclude', function( $js_excl, $html ) {
+                $revslider_excl = 'revslider, setREVStartSize, jquery.min.js';
+                if ( false !== strpos( $html, 'setREVStartSize' ) ) {
+                    if ( is_array( $js_excl ) ) {
+                        $js_excl = implode( ',', $js_excl );
+                    }
+                    
+                    $js_excl .= ',' . $revslider_excl;
+                }
+                return $js_excl;
+            }, 10, 2 );
+        }
+        
+        // stop divi from purging cache -> likely in autoptimizeCache.php instead.
+    }
+    
+    public function revslider_compat_helper() {
+        if ( ( $this->conf->get( 'autoptimize_js_aggregate' ) || apply_filters( 'autoptimize_filter_js_dontaggregate', false ) ) && apply_filters( 'autoptimize_js_include_inline', $this->conf->get( 'autoptimize_js_include_inline' ) ) ) {
+            // if all files including inline JS are aggregated we don't need to exclude revslider stuff.
+            return false;
+        } else if ( apply_filters( 'autoptimize_filter_js_defer_not_aggregate', $this->conf->get( 'autoptimize_js_defer_not_aggregate' ) ) && apply_filters( 'autoptimize_js_filter_defer_inline', $this->conf->get( 'autoptimize_js_defer_inline' ) ) ) {
+            // and when not aggregating but deferring all excluding is not needed either.
+            return false;
+        }
+        // in all other cases we need to exclude the revslider stuff.
+        return true;
     }
 }
