a0f87112fe80b5a97b036229c41cb18454392858
futtta <futtta@gmail.com>
2026-02-17 21:23:03 +0100

    why preg_replace if you can simply str_replace ;)

--- classes/autoptimizeImages.php
+++ classes/autoptimizeImages.php
@@ -984,8 +984,9 @@ class autoptimizeImages
                 $placeholder = apply_filters( 'autoptimize_filter_imgopt_lazyload_placeholder', $this->get_default_lazyload_placeholder( $width, $height ) );
             }
 
-            $tag = preg_replace( '/(\s)src=/', ' src=\'' . $placeholder . '\' data-src=', $tag, 1 );
-            $tag = preg_replace( '/(\s)srcset=/', ' data-srcset=', $tag, 1 );
+            $tag = str_replace( ' src=', ' data-src=', $tag );
+            $tag = str_replace( ' srcset=', ' data-srcset=', $tag );
+            $tag = str_replace( '<img ', '<img src="' . $placeholder . '" ', $tag );
 
             // move sizes to data-sizes unless filter says no.
             if ( apply_filters( 'autoptimize_filter_imgopt_lazyload_move_sizes', true ) ) {
